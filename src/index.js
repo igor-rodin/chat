08012e895300291fafbd7744c2b9d35f0be8f1af
Igor Rodin <igreen1974@gmail.com>
2021-08-28 00:12:26 +0300

    Выпускной проект чат

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import './scss/main.scss'
 import { createLogin } from './js/login.js'
-import { avatar } from './images/abstract-user.svg'
+import { creatChat } from './js/chat.js'
+import avatarDefault from './images/abstract-user.svg'
+import { createUser } from './js/user'
+import { createMessage } from './js/message'
+import { createAvatarForm } from './js/avatar-form.js'
 
 document.addEventListener('DOMContentLoaded', () => {
+
+  const appContainer = document.querySelector('#container');
 
   const login = createLogin({
     title: 'Авторизация',
@@ -12,16 +18,212 @@ document.addEventListener('DOMContentLoaded', () => {
     buttonText: 'Войти'
   })
 
-  document.querySelector('#container').append(login);
+  appContainer.append(login);
 
   document.addEventListener('click', (event) => {
     if (event.target.id === 'logInBtn') {
       const nickName = document.querySelector('#nickName').value.trim();
       const fio = document.querySelector('#fio').value.trim();
       if (nickName && fio) {
-        console.log(nickName, fio);
         login.remove();
+        const loginTime = getCurrentTime();
+        const activeUser = {
+          name: fio,
+          nickName: nickName,
+          avatar: avatarDefault,
+          loginTime: loginTime,
+        };
+
+        const chatScreen = creatChat({
+          activeUserFio: activeUser.name,
+          activeUserNickname: activeUser.nickName,
+          avatar: activeUser.avatar,
+        });
+
+        appContainer.append(chatScreen);
+
+
+        const sendBtn = appContainer.querySelector('#sendBtn');
+        sendBtn.addEventListener('click', (event) => {
+          const inputMsg = appContainer.querySelector('#inputMessage');
+          const msgText = inputMsg.value.trim();
+          sendMessage(msgText);
+          inputMsg.value = '';
+        });
+
+        const avatarLink = appContainer.querySelector('#loadLink');
+
+        avatarLink.addEventListener('click', (event) => {
+          event.preventDefault();
+          const avatarForm = createAvatarForm({
+            avatar: activeUser.avatar
+          });
+
+          document.body.append(avatarForm);
+
+          const cancelBtn = avatarForm.querySelector('#cancelBtn');
+          if (cancelBtn) {
+            cancelBtn.addEventListener('click', (event) => {
+              avatarForm.remove();
+            })
+          }
+
+          const loadBtn = avatarForm.querySelector('#avatarFileInput');
+          const saveBtn = avatarForm.querySelector('#saveBtn');
+          const avatarImg = avatarForm.querySelector('#avatarImg');
+          const fileReader = new FileReader();
+
+          fileReader.addEventListener('load', () => {
+            avatarImg.src = fileReader.result;
+          })
+
+          loadBtn.addEventListener('change', (event) => {
+            const file = event.target.files[0];
+            if (file) {
+              fileReader.readAsDataURL(file);
+            }
+
+          });
+
+          saveBtn.addEventListener('click', (event) => {
+            activeUser.avatar = avatarImg.src;
+            const activeUserElem = document.querySelector('#activeUserImg');
+            if (activeUserElem) {
+              activeUserElem.src = avatarImg.src;
+            }
+            syncAvatar(avatarImg.src)
+            avatarForm.remove();
+          })
+
+        });
+
+        avatarLink.addEventListener('dragover', (event) => {
+          event.preventDefault();
+        })
+
+        avatarLink.parentElement.addEventListener('drop', (event) => {
+          event.preventDefault();
+          const dataDroped = event.dataTransfer;
+          if (dataDroped.files && dataDroped.files.length) {
+            const fileReader = new FileReader();
+            fileReader.readAsDataURL(dataDroped.files[0]);
+            fileReader.addEventListener('load', () => {
+              activeUser.avatar = fileReader.result;
+              avatarLink.querySelector('#activeUserImg').src = activeUser.avatar;
+              syncAvatar(activeUser.avatar);
+            })
+          }
+        })
+
+        const ws = new WebSocket("ws://localhost:8000/");
+
+        ws.onopen = (e) => {
+          const message = {
+            type: 'connection',
+            user: activeUser,
+          }
+
+          ws.send(JSON.stringify(message));
+        };
+
+        ws.onmessage = (message) => {
+          const { users, messages } = JSON.parse(message.data);
+
+          if (users.length) {
+            appContainer.querySelector('#userCnt').textContent = users.length;
+            updateUsers(appContainer, users);
+          }
+
+          if (messages.length) {
+            if (messages.length === 1 && messages[0].type === 'avatar') {
+              updateAvatar(appContainer, messages[0].id, messages[0].avatarSrc);
+            }
+            else {
+              updateMesages(appContainer, messages);
+            }
+          }
+        };
+
+
+        ws.addEventListener('error', function (ev) {
+          console.log('Соединение закрыто или не может быть открыто');
+        });
+
+        function syncAvatar(avatar) {
+          const message = {
+            type: 'avatar',
+            user: activeUser,
+          };
+          ws.send(JSON.stringify(message))
+        }
+
+        function getCurrentTime() {
+          return new Date().toTimeString().slice(0, 5);
+        }
+
+        function sendMessage(messageText) {
+          activeUser.loginTime = getCurrentTime();
+          const message = {
+            type: 'user',
+            user: activeUser,
+            message: messageText
+          };
+          ws.send(JSON.stringify(message));
+        }
       }
     }
   })
+
+  function updateAvatar(container, userId, avatarSrc) {
+    const targets = document.querySelectorAll('.user');
+    targets.forEach((target) => {
+      if (target.dataset.userId === userId) {
+        target.querySelector('img').src = avatarSrc;
+      }
+    })
+  };
+
+  function updateUsers(container, users) {
+    const usersList = container.querySelector('#users');
+    usersList.innerHTML = '';
+    users.forEach((elem) => {
+      const liElem = createUser({
+        userId: elem.id,
+        avatar: elem.user.avatar,
+        userName: elem.user.name,
+      })
+
+      usersList.append(liElem);
+    });
+    usersList.scrollTop = usersList.scrollHeight;
+  };
+
+  function updateMesages(container, messages) {
+    const msgList = container.querySelector('#msgList');
+    messages.forEach((msg) => {
+      if (msg.type === 'connection') {
+        const msgElem = createMessage({
+          userMessage: false,
+          avatar: msg.user.avatar,
+          userName: msg.user.name,
+          msgText: msg.message,
+        });
+
+        msgList.append(msgElem);
+      }
+      else if (msg.type === 'user') {
+        const msgElem = createMessage({
+          userMessage: true,
+          userId: msg.user.id,
+          avatar: msg.user.avatar,
+          userName: msg.user.name,
+          msgText: msg.message,
+          msgTime: msg.user.loginTime,
+        });
+
+        msgList.append(msgElem);
+      }
+    })
+    msgList.parentElement.scrollTop = msgList.parentElement.scrollHeight;
+  };
 })
--- src/server/server.js
+++ src/server/server.js
@@ -1,1 +1,67 @@
-console.log('Server started');
+const WebSocketServer = new require('ws');
+const { v4: uuidv4 } = require('uuid');
+
+let clients = {};
+const users = [];
+const messages = [];
+const webSocketServer = new WebSocketServer.Server({ port: 8000 }, () => {
+  console.log('Сервер запущен на порту 8000');
+});
+
+webSocketServer.on('connection', function (ws) {
+  const clientId = uuidv4();
+  clients[clientId] = ws;
+
+  ws.send(JSON.stringify({ users: users, messages: messages }));
+
+  ws.on('message', function (receivedMsg) {
+    const { type, user, message } = JSON.parse(receivedMsg);
+
+
+    switch (type) {
+      case 'connection':
+        const msgText = `${user.name} (${user.nickName}) вошел в чат`;
+        users.push({ id: clientId, type: type, user: user });
+        messages.push({ id: clientId, type: type, user: user, message: msgText });
+        sendBroadcastMessage(JSON.stringify({
+          users: users,
+          messages: [{ id: clientId, type: type, user: user, message: msgText }],
+        }));
+        break;
+      case 'user':
+        messages.push({ id: clientId, type: type, user: user, message: message });
+        sendBroadcastMessage(JSON.stringify({
+          users: [],
+          messages: [{ id: clientId, type: type, user: user, message: message }],
+        }));
+        break;
+      case 'avatar':
+        debugger;
+        sendBroadcastMessage(JSON.stringify({
+          users: [],
+          messages: [{ id: clientId, type: type, avatarSrc: user.avatar }],
+        }));
+        break;
+    }
+  });
+
+  ws.on('close', function () {
+    const closedIdx = users.findIndex((user) => user.id === clientId);
+    const msgText = `${users[closedIdx].user.name} (${users[closedIdx].user.nickName}) вышел из чата`;
+    messages.push({ id: clientId, type: 'connection', user: users[closedIdx].user, message: msgText });
+
+    sendBroadcastMessage(JSON.stringify({
+      users: users,
+      messages: [{ id: clientId, type: 'connection', user: users[closedIdx].user, message: msgText }]
+    }));
+
+    users.splice(closedIdx, 1);
+    delete clients[clientId];
+  });
+
+  function sendBroadcastMessage(message) {
+    for (const id in clients) {
+      clients[id].send(message);
+    }
+  };
+});
